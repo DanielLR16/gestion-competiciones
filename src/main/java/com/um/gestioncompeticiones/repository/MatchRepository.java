074311ba18b0f9559e06902e66c127cb18be6441
DanielLR16 <daniellr_16@hotmail.com>
2025-10-13 20:43:34 +0200

    Servicio Match.

--- src/main/java/com/um/gestioncompeticiones/repository/MatchRepository.java
+++ src/main/java/com/um/gestioncompeticiones/repository/MatchRepository.java
@@ -4,7 +4,15 @@ import com.um.gestioncompeticiones.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MatchRepository  extends JpaRepository<Match, Long> {
 
+    // Obtener todos los partidos de una competición
+    List<Match> findByCompetitionId(Long competitionId);
+
+    // Obtener los partidos de una competición en una fecha concreta (útil para primera jornada)
+    List<Match> findByCompetitionIdAndMatchDate(Long competitionId, LocalDate matchDate);
 }
