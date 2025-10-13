a49ce068be38934b6d9368c9e02ba1a59ff7cd02
DanielLR16 <daniellr_16@hotmail.com>
2025-10-13 19:29:42 +0200

    Creación del proyecto.

---
+++ src/test/java/com/um/gestioncompeticiones/GestionCompeticionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um.gestioncompeticiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCompeticionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
